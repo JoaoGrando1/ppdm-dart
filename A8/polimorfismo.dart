3c7c854ba5bad4d7e409766a4ecc137df9af2fc4
JoaoGrando1 <134527296+JoaoGrando1@users.noreply.github.com>
2024-03-27 08:57:11 -0300

    Add files via upload

---
+++ A8/polimorfismo.dart
@@ -0,0 +1,33 @@
+import 'classes.dart';
+
+void main() {
+  Cachorro cachorro = new Cachorro();
+  cachorro.emitirSom();
+  cachorro.dormir();
+  cachorro.caminhar();
+  cachorro.correr();
+
+  Leao leao = new Leao();
+  leao.emitirSom();
+  leao.dormir();
+  leao.caminhar();
+  leao.correr();
+
+  Lobo lobo = new Lobo();
+  lobo.emitirSom();
+  lobo.dormir();
+  lobo.caminhar();
+  lobo.correr();
+
+  Tigre tigre = new Tigre();
+  tigre.emitirSom();
+  tigre.dormir();
+  tigre.caminhar();
+  tigre.correr();
+
+  Gato gato = new Gato();
+  gato.emitirSom();
+  gato.dormir();
+  gato.caminhar();
+  gato.correr();
+}
